43b7c56d13396acfc59fc28fda4f52a19d522725
Lazare-42 <lazrossi@student.42.fr>
2019-12-14 14:49:44 +0100

    version with up arrows and down arrows malfunctionning : it seems every strings is stored twice

--- Main.hs
+++ Main.hs
@@ -28,15 +28,15 @@ moveBackOneChar (rowSize,colSize) = do
 
 eraseLastChar :: Window -> ScreenSize -> Curses ()
 eraseLastChar w screenSize = do
-                            updateWindow w (moveBackOneChar screenSize)
+                            updateWindow w $ moveBackOneChar screenSize
                             showInput w " "
-                            updateWindow w (moveBackOneChar screenSize)
+                            updateWindow w $ moveBackOneChar screenSize
                             render
 
 eraseWholeString :: Window -> ScreenSize -> String -> Curses ()
 eraseWholeString w screenSize (x:xs) = do 
                                       eraseLastChar w screenSize
-                                      eraseWholeString w screenSize (init (x:xs))
+                                      eraseWholeString w screenSize $ init (x:xs)
 eraseWholeString w screenSize [] = return ()
 
 evaluator :: CurrentState -> ScreenSize -> Window -> Curses ()
@@ -47,43 +47,59 @@ evaluator (line:xs, varlist) screenSize w = do
     Let var exp -> do
       let varlist' = varlist #+ (var, exp)
       showInput w ((pretty $ eval exp varlist') ++ "\n")
-      buildLine ("":(line:xs), varlist') screenSize w
+      buildLine  0 ("":(line:xs), varlist') screenSize w
     _ -> do
       showInput w ((pretty $ eval exp varlist) ++ "\n")
-      buildLine ("":(line:xs), varlist) screenSize w
+      buildLine  0 ("":(line:xs), varlist) screenSize w
 
-buildLine :: CurrentState -> ScreenSize -> Window -> Curses ()
-buildLine (str:x,variables) screenSize w = do
-                          ev <- getEvent w Nothing
-                          case ev of
-                            Just (EventCharacter c) -> case c of
-                                                         '\n' -> do
-                                                            showInput w [c]
-                                                            evaluator (str:x, variables) screenSize w
-                                                         c -> do 
-                                                            showInput w [c]
-                                                            buildLine ((str ++ [c]):x,variables) screenSize w
-                            Just (EventSpecialKey k) -> case k of 
-                                                          KeyUpArrow -> do
-                                                            eraseWholeString w screenSize str
-                                                            showInput w (head x)
-                                                            buildLine (x,variables) screenSize w
-                                                          KeyBackspace ->
-                                                            case str of
-                                                              [] -> buildLine ((str):x,variables) screenSize w
-                                                              str -> do
-                                                                eraseLastChar w screenSize
-                                                                buildLine ((tail str):x,variables) screenSize w
-                                                          k -> do
-                                                            showInput w (show k)
-                                                            buildLine (str:x,variables) screenSize w
-                            _ -> buildLine (str:x,variables) screenSize w
-                          return ()
+increaseListBrowser :: Int -> [String] -> Int
+increaseListBrowser num strings = if num >= (length strings - 1)
+                                        then (length strings) - 1
+                                        else (num + 1)
+
+replaceModifiedString :: Int -> String -> [String] -> [String]
+replaceModifiedString num firstStr (x:xs) = if num /= 0
+                                              then (firstStr:(x:xs))
+                                              else (firstStr:(xs))
+
+buildLine  :: Int -> CurrentState -> ScreenSize -> Window -> Curses ()
+buildLine  num (strings,variables) screenSize w = do
+  let str = strings !! num
+  ev <- getEvent w Nothing
+  case ev of
+    Just (EventCharacter c) -> case c of
+                                 '\n' -> do
+                                    showInput w [c]
+                                    case str of
+                                      "" -> buildLine  0 (strings,variables) screenSize w
+                                      _ -> evaluator ((str:strings), variables) screenSize w
+                                 c -> do 
+                                    showInput w [c]
+                                    buildLine  0 ((replaceModifiedString num (str ++ [c]) strings),variables) screenSize w
+    Just (EventSpecialKey k) -> case k of 
+                                  KeyUpArrow -> do
+                                               let newNum = increaseListBrowser num strings
+                                               eraseWholeString w screenSize str
+                                               showInput w $ strings !! newNum
+                                               buildLine newNum (strings,variables) screenSize w
+                                  KeyDownArrow -> do
+                                               let newNum = (\x -> if x /= 0 then x - 1 else x) num
+                                               eraseWholeString w screenSize str
+                                               showInput w $ strings !! newNum
+                                               buildLine newNum (strings,variables) screenSize w
+                                  KeyBackspace ->
+                                    case str of
+                                      [] -> buildLine  0 ("":strings,variables) screenSize w
+                                      str -> do
+                                        eraseLastChar w screenSize
+                                        buildLine  0 ((tail str):strings,variables) screenSize w
+                                  _ -> buildLine  0 (strings,variables) screenSize w
+  return ()
 
 main :: IO ()
 main = runCurses $ do
           setEcho False
           w <- defaultWindow
           wSize <- screenSize
-          buildLine ([""],empty) wSize w 
+          buildLine  0 ([""],empty) wSize w 
           closeWindow w
