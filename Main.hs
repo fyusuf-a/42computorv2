eb806d276be8990014ca02855d6cfab49cdb8e11
fyusuf-a <fyusuf-a@student.42.fr>
2019-12-15 19:15:04 +0100

    Implemented haskeline

--- Main.hs
+++ Main.hs
@@ -1,28 +1,37 @@
 module Main where
 
-import Tokens
-import Data.Map(empty)
-import Lexer
-import Eval
-import Control.Monad
-import Useful.Dictionary
-import Debug.Trace
-import Complex
+import Tokens (calc, Exp(..))
+import Data.Map (empty)
+import Lexer (lexer)
+import Eval (eval, VarList)
+import Useful.Dictionary ((#+))
+import Complex (pretty)
+import System.Console.Haskeline (getInputLine, defaultSettings)
+import System.Console.Haskeline.IO (InputState, initializeInput
+          , cancelInput, queryInput)
+import Control.Exception (bracket)
 
-mainLoop:: VarList -> IO ()
-mainLoop list = do
-  line <- getLine 
-  let exp = calc . lexer $ line
-  case exp of
-    Let var exp -> do
-      let list' = list #+ (var, exp)
-      {-putStrLn . show $ exp-}
-      putStrLn . pretty $ eval exp list'
-      mainLoop list'
-    _ -> do
-      {-putStrLn . show $ exp-}
-      putStrLn . pretty $ eval exp list
-      mainLoop list
+mainLoop:: InputState -> VarList -> IO ()
+mainLoop is list = do
+  input <- queryInput is (getInputLine "") 
+  case input of
+    Nothing -> mainLoop is list
+    Just "quit" -> return ()
+    Just line -> do
+      let exp = calc . lexer $ line
+      case exp of
+        Let var exp -> do
+          let list' = list #+ (var, exp)
+          {-putStrLn . show $ exp-}
+          putStrLn . pretty $ eval exp list'
+          mainLoop is list'
+        _ -> do
+          {-putStrLn . show $ exp-}
+          putStrLn . pretty $ eval exp list
+          mainLoop is list
 
 main :: IO ()
-main = mainLoop empty
+main = bracket
+          (initializeInput defaultSettings)
+          cancelInput
+          (\is -> mainLoop is empty)
